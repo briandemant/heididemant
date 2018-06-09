35d0f1cca0ebb7e522fe10c467cc84a76477ce48
Brian Demant <brian.demant@gmail.com>
2018-06-09 16:17:39 +0200

    added recommendation

--- gulpfile.js
+++ gulpfile.js
@@ -1,55 +1,42 @@
-var gulp = require("gulp")
-var browserSync = require("browser-sync").create()
-var less = require("gulp-less")
-var nodemon = require("gulp-nodemon")
-var cleanCSS = require("gulp-clean-css")
-var purify = require("gulp-purifycss")
+var gulp = require('gulp')
+var browserSync = require('browser-sync').create()
+var less = require('gulp-less')
+var nodemon = require('gulp-nodemon')
+var cleanCSS = require('gulp-clean-css')
+var purify = require('gulp-purifycss')
 
-const stylePath = "src/style/"
+const stylePath = 'src/style/'
 
-gulp.task("serve", ["less", "nodemon", "watch"], function() {
-	browserSync.init(null, {
-		ui: false,
-		open: false,
-		// scrollThrottle : 100,
-		ghostMode: { scroll: false },
-		reloadOnRestart: true,
-		proxy: "http://localhost:8080",
-		files: ["public/**/*.*"],
-		port: 3000,
-	})
+gulp.task('serve', ['less', 'watch'], function () {
+	browserSync.init({
+		                 ui: false,
+		                 open: false,
+		                 // scrollThrottle : 100,
+		                 ghostMode: { scroll: false },
+		                 reloadOnRestart: true,
+		                 server: {
+			                 baseDir: './public',
+		                 },
+	                 })
+	gulp.watch('public/*.html')
+		.on('change', browserSync.reload)
+
 })
 
 // Compile less into CSS & auto-inject into browsers
-gulp.task("less", function() {
+gulp.task('less', function () {
 	return (gulp
-			.src(stylePath + "style.less")
-			.pipe(less())
-			.pipe(cleanCSS({ compatibility: "ie9" }))
-			// .pipe(purify(['./public/js/*.js', './public/**/*.html']))
-			.pipe(gulp.dest("public/css"))
-			.pipe(browserSync.stream()) )
+		.src(stylePath + 'style.less')
+		.pipe(less())
+		.pipe(cleanCSS({ compatibility: 'ie9' }))
+		// .pipe(purify(['./public/js/*.js', './public/**/*.html']))
+		.pipe(gulp.dest('public/css'))
+		.pipe(browserSync.stream()))
 })
 
-gulp.task("nodemon", function(cb) {
-	var started = false
-
-	return nodemon({
-		script: "index.js",
-		ignore: ["public", "src/style", "src/client"],
-	}).on("start", function() {
-		// to avoid nodemon being started multiple times
-		// thanks @matthisk
-		if (!started) {
-			cb()
-			started = true
-		}
-	})
+gulp.task('watch', function () {
+	gulp.watch(stylePath + '*.less', ['less'])
 })
 
-gulp.task("watch", function() {
-	gulp.watch(stylePath + "*.less", ["less"])
-})
-
-gulp.task("build", ["less"])
-gulp.task("default", ["serve"])
+gulp.task('build', ['less'])
+gulp.task('default', ['serve'])
--- public/js/custom.js
+++ public/js/custom.js
@@ -1,58 +1,75 @@
 function chooseNav($anchor) {
-	$('.navbar-nav li').removeClass('active');
-	$anchor.closest('li').addClass('active');
-};
+	$('.navbar-nav li').removeClass('active')
+	$anchor.closest('li').addClass('active')
+}
 
 
 function goto(name) {
-	var $target = $("[name='" + name + "']");
-	window.$target = $target;
+	var $target = $('[name=\'' + name + '\']')
+	window.$target = $target
 	if ($target.length) {
 		$('html, body').stop().animate({
-			scrollTop: $target.offset().top
-		}, 1000, 'easeInOutExpo');
+			                               scrollTop: $target.offset().top,
+		                               }, 1000, 'easeInOutExpo')
 	}
 }
 
 function gotoId(id) {
-	var $target = $("#" + id);
-	window.$target = $target;
+	var $target = $('#' + id)
+	window.$target = $target
 	if ($target.length) {
 		$('html, body').stop().animate({
-			scrollTop: $target.offset().top - 85
-		}, 1000, 'easeInOutExpo')
+			                               scrollTop: $target.offset().top - 85,
+		                               }, 1000, 'easeInOutExpo')
 	}
 }
 
 function highlight(id) {
-	var $target = $("#" + id);
-	window.$target = $target;
-	$target.removeClass("highlight");
-	setTimeout(function() {
-		$target.addClass("highlight");
-	}, 200);
-};
-$(function() {
-	$('.navbar-nav li a').bind('click', function(event) {
-		event.preventDefault();
-		var $anchor = $(this);
-		chooseNav($anchor);
-		var name = $anchor.attr('href').slice(1);
+	var $target = $('#' + id)
+	window.$target = $target
+	$target.removeClass('highlight')
+	setTimeout(function () {
+		$target.addClass('highlight')
+	}, 200)
+}
 
-		goto(name);
+/// map
+function initMap() {
+	var uluru = { lat: 55.3783002, lng: 10.415543699999944 }
+	var map = new google.maps.Map(document.getElementById('gmap'), {
+		zoom: 15,
+		center: uluru,
+	})
+	var marker = new google.maps.Marker({
+		                                    position: uluru,
+		                                    map: map,
+	                                    })
+}
+
+$(function () {
+	$('.navbar-nav li a').bind('click', function (event) {
+		event.preventDefault()
+		var $anchor = $(this)
+		chooseNav($anchor)
+		var name = $anchor.attr('href').slice(1)
+
+		goto(name)
 
 		if ($('.navbar .in').length) {
 			$('.navbar-toggle').click()
 		}
-	});
+	})
 
-	var active = document.location.hash;
+	var active = document.location.hash
 	if (active) {
-		var anchor = $("[href='" + active + "']");
-		chooseNav($(anchor));
+		var anchor = $('[href=\'' + active + '\']')
+		chooseNav($(anchor))
 	}
 
-   // 
-	$('#contact').on('shown.bs.modal', initMap);
-	$('#seek').modal('show');
-});
+	//
+	$('#contact').on('shown.bs.modal', initMap)
+	if (document.location.hostname !== 'localhost') {
+		$('#seek').modal('show')
+	}
+})
+
